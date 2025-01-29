bc17745f93e64b78fe3769736335653c3f2c13b1
Thijs-Desjardijn <thijsproo@gmail.com>
2025-01-29 21:49:06 +0100

    added player to asteroids

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 import pygame
-import constants
 from constants import *
-import pygame
-
-# Base class for game objects
-class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-        # we will be using this later
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
-
-    def draw(self, screen):
-        # sub-classes must override
-        pass
-
-    def update(self, dt):
-        # sub-classes must override
-        pass
+from player import Player
 def main():
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 0 
@@ -34,6 +15,7 @@ def main():
 			if event.type == pygame.QUIT:
 				gameExit = True
 		screen.fill((0,0,0)) 
+		player.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 if __name__ == "__main__":
